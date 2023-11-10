b5387db8375801e5ebc45ed8a17c880b53e2904e
Sakina Juzer <sakinajuz136@gmail.com>
2023-11-10 12:32:45 +0400

    added all files

---
+++ products.js
@@ -0,0 +1,21 @@
+let products =[
+    {
+        id:1001,
+        title:"Crochet",
+        description: "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+        price: 20.00,
+        image:"images/crochet.jpeg",
+        availableInventory:10,
+        rating: 2
+    
+    },
+    {
+        id:1002,
+        title:"Cat food 25lb bag",
+        description: "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+        price: 50.00,
+        image:"images/catfood.jpg",
+        availableInventory:10,
+        rating: 2
+    },
+]
