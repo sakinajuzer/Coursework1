7093b50b99c999683b020e53f001371b7fbdca3c
Sakina Juzer <sakinajuz136@gmail.com>
2023-11-10 15:25:59 +0400

    products added

--- products.js
+++ products.js
@@ -2,20 +2,94 @@ let products =[
     {
         id:1001,
         title:"Crochet",
-        description: "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+        description: "IEHFIJODIHEF",
         price: 20.00,
         image:"images/crochet.jpeg",
-        availableInventory:10,
+        availableInventory: 10,
+        rating: 2
+    
+    },
+    {
+        id:1002,
+        title:"art",
+        description: "f dsf dsf dsf ds fsd fds gd",
+        price: 50.00,
+        image:"images/artclass.jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id:1001,
+        title:"Crochet",
+        description: "IEHFIJODIHEF",
+        price: 20.00,
+        image:"images/horse.jpg",
+        availableInventory: 10,
         rating: 2
     
     },
     {
         id:1002,
         title:"Cat food 25lb bag",
-        description: "IEHFIJODIHEFWOLMKIHFDWIOEJF",
+        description: "f dsf dsf dsf ds fsd fds gd",
         price: 50.00,
-        image:"images/catfood.jpg",
-        availableInventory:10,
+        image:"images/candle.png",
+        availableInventory: 10,
         rating: 2
     },
+    {
+        id:1001,
+        title:"Crochet",
+        description: "IEHFIJODIHEF",
+        price: 20.00,
+        image:"images/soap.png",
+        availableInventory: 10,
+        rating: 2
+    
+    },
+    {
+        id:1002,
+        title:"Cat food 25lb bag",
+        description: "f dsf dsf dsf ds fsd fds gd",
+        price: 50.00,
+        image:"images/resin.jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id:1002,
+        title:"Cat food 25lb bag",
+        description: "f dsf dsf dsf ds fsd fds gd",
+        price: 50.00,
+        image:"images/potterymaking.avif",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id:1002,
+        title:"Cat food 25lb bag",
+        description: "f dsf dsf dsf ds fsd fds gd",
+        price: 50.00,
+        image:"images/emb.png",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id:1002,
+        title:"Cat food 25lb bag",
+        description: "f dsf dsf dsf ds fsd fds gd",
+        price: 50.00,
+        image:"images/poetry.jpg",
+        availableInventory: 10,
+        rating: 2
+    },
+    {
+        id:1002,
+        title:"Cat food 25lb bag",
+        description: "f dsf dsf dsf ds fsd fds gd",
+        price: 50.00,
+        image:"images/makeup.jpg",
+        availableInventory: 10,
+        rating: 2
+    }
 ]
