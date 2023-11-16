d3522fc27627ec1d1b6830961b88643c96515fcb
Sakina Juzer <sakinajuz136@gmail.com>
2023-11-16 16:37:10 +0400

    Fixed errors

--- products.js
+++ products.js
@@ -1,95 +1,95 @@
-let products =[
+let products = [
     {
-        id:1001,
-        title:"Crochet Classes",
+        id: 1001,
+        title: "Crochet Classes",
         description: "Learn the art of needle and yarn through crocheting !!!",
-        price: "AED 200",
-        image:"images/crochet.jpeg",
+        price: 200,
+        image: "images/crochet.jpeg",
         availableInventory: 10,
-        rating: 5
-    
+        location: "Abu Dhabi"
+
     },
     {
-        id:1002,
-        title:"Art Classes",
+        id: 1002,
+        title: "Art Classes",
         description: "Find yourself playing with colours !!!",
-        price: "AED 250",
-        image:"images/artclass.jpg",
+        price: 250,
+        image: "images/artclass.jpg",
         availableInventory: 10,
-        rating: 4
+        location: "Jumeirah"
     },
     {
-        id:1003,
-        title:"Horse Riding Classes",
+        id: 1003,
+        title: "Horse Riding Classes",
         description: "Explore the world on a Horseback !!!",
-        price: "AED 400",
-        image:"images/horse.jpg",
+        price: 400,
+        image: "images/horse.jpg",
         availableInventory: 10,
-        rating: 5
-    
+        location: "Jabal Ali"
+
     },
     {
-        id:1004,
-        title:"Cat food 25lb bag",
+        id: 1004,
+        title: "Candle Making Classes",
+        description: "Learn how to make your favourite scented candle your own way",
+        price: 500,
+        image: "images/candle.png",
+        availableInventory: 10,
+        location: "Barsha"
+    },
+    {
+        id: 1005,
+        title: "Soap Making Classes",
+        description: "Learn what's best suited for your skin and make your own soap",
+        price: 300,
+        image: "images/soap.png",
+        availableInventory: 10,
+        location: "Sharjah"
+
+    },
+    {
+        id: 1006,
+        title: "Resin Art Classes",
+        description: "Pour, Create, Inspire: Learn Resin Art to make your creations",
+        price: 50.00,
+        image: "images/resin.jpg",
+        availableInventory: 10,
+        location: "Al Ain"
+    },
+    {
+        id: 1007,
+        title: "Pottery Making Classes",
+        description: "Transform Clay into Art: Experience the joy of Pottery Making",
+        price: 350,
+        image: "images/potterymaking.avif",
+        availableInventory: 10,
+        location: "Al Qouz"
+    },
+    {
+        id: 1008,
+        title: "Embroidery Classes",
+        description: "From knots to beauty, Learn the art of Embroidery",
+        price: 200,
+        image: "images/emb.png",
+        availableInventory: 10,
+        location: "Abu Dhabi"
+    },
+    {
+        id: 1009,
+        title: "Poetry Classes",
         description: "f dsf dsf dsf ds fsd fds gd",
         price: 50.00,
-        image:"images/candle.png",
+        image: "images/poetry.jpg",
         availableInventory: 10,
-        rating: 2
+        location: "Dubai"
     },
     {
-        id:1005,
-        title:"Crochet",
-        description: "IEHFIJODIHEF",
-        price: 20.00,
-        image:"images/soap.png",
-        availableInventory: 10,
-        rating: 2
-    
-    },
-    {
-        id:1006,
-        title:"Cat food 25lb bag",
-        description: "f dsf dsf dsf ds fsd fds gd",
+        id: 1010,
+        title: "MakeUp Classes",
+        description: "From basics to beauty: Learn the art of makeup thorugh every stroke",
         price: 50.00,
-        image:"images/resin.jpg",
+        image: "images/makeup.jpg",
         availableInventory: 10,
-        rating: 2
-    },
-    {
-        id:1007,
-        title:"Cat food 25lb bag",
-        description: "f dsf dsf dsf ds fsd fds gd",
-        price: 50.00,
-        image:"images/potterymaking.avif",
-        availableInventory: 10,
-        rating: 2
-    },
-    {
-        id:1008,
-        title:"Cat food 25lb bag",
-        description: "f dsf dsf dsf ds fsd fds gd",
-        price: 50.00,
-        image:"images/emb.png",
-        availableInventory: 10,
-        rating: 2
-    },
-    {
-        id:1009,
-        title:"Cat food 25lb bag",
-        description: "f dsf dsf dsf ds fsd fds gd",
-        price: 50.00,
-        image:"images/poetry.jpg",
-        availableInventory: 10,
-        rating: 2
-    },
-    {
-        id:1010,
-        title:"Cat food 25lb bag",
-        description: "f dsf dsf dsf ds fsd fds gd",
-        price: 50.00,
-        image:"images/makeup.jpg",
-        availableInventory: 10,
-        rating: 2
+        location: "Sharjah"
     }
 ]
